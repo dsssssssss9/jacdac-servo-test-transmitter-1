ff768f2697d63a983b25822df79c02d1c1d6cf20
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-07-29 12:20:43 +0100

    set radio group. Basic maths to keep track of rotory encoder posistion

--- main.ts
+++ main.ts
@@ -1,7 +1,16 @@
+modules.rotaryEncoder1.onPositionChanged(function () {
+    Pos = Pos + (modules.rotaryEncoder1.position() - Old_Pos)
+    Old_Pos = Pos
+})
 modules.button1.onEvent(jacdac.ButtonEvent.Down, function () {
     music.playSoundEffect(music.builtinSoundEffect(soundExpression.happy), SoundExpressionPlayMode.UntilDone)
+    basic.showNumber(Pos)
 })
+let Old_Pos = 0
+let Pos = 0
+radio.setGroup(55)
+Pos = 0
 basic.showIcon(IconNames.Yes)
 basic.forever(function () {
-    basic.showNumber(modules.rotaryEncoder1.position())
+	
 })
