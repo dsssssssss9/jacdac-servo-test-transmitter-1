17b636ea6088a743b6940b8dfd619474eb528006
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-07-29 14:12:13 +0100

    add difference of old rotor

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 modules.rotaryEncoder1.onPositionChanged(function () {
-    Pos = Pos + (modules.rotaryEncoder1.position() - Old_Pos)
+    Pos = Old_Pos - (Old_Rot - modules.rotaryEncoder1.position())
     Old_Pos = Pos
+    Old_Rot = modules.rotaryEncoder1.position()
 })
 modules.button1.onEvent(jacdac.ButtonEvent.Down, function () {
     basic.showNumber(Pos)
@@ -9,10 +10,12 @@ modules.button2.onEvent(jacdac.ButtonEvent.Down, function () {
     music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 1000, 999, 255, 0, 471, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
     radio.sendNumber(Pos)
 })
+let Old_Rot = 0
 let Old_Pos = 0
 let Pos = 0
 radio.setGroup(55)
-Pos = 0
+Pos = 90
+Old_Pos = Pos
 basic.showIcon(IconNames.Yes)
 basic.forever(function () {
 	
